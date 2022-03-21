2b5949e90c5614a9b2141d2e9b39f10f4b36e75e
Nour Goulmamine <ng9sc@virginia.edu>
2022-03-21 19:35:04 -0400

    Update gameover.php

---
+++ templates/gameover.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">  
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="CS4640">
+        <meta name="description" content="CS4640 Trivia Login Page">  
+        <title>Trivia Game Login</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"> 
+    </head>
+    <body>
+        <div class="container" style="margin-top: 15px;">
+            <div class="row col-xs-8">
+                <p> GameOverPage</p>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-4">
+                <?php
+                    if (!empty($error_msg)) {
+                        echo "<div class='alert alert-danger'>$error_msg</div>";
+                    }
+                ?>
+                <h3>Hello <?=$_SESSION["name"]?>! Guesses: <?=$_SESSION["guesses"]?></h3>
+                <h3>The word was <?=$_SESSION["answer"]?>!</h3>
+                </div>
+                <a href="?command=question" class="btn btn-danger">Play again</a>
+                <a href="?command=logout" class="btn btn-danger">Exit</a>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    </body>
+</html>
